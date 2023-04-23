47dafa9fa61502aab869f1527dee060da4c99020
JN <jared.bozh@gmail.com>
2023-04-24 01:19:38 +0800

    rev router

--- src/core/router/Router.js
+++ src/core/router/Router.js
@@ -1,55 +1,55 @@
-import { $ } from "../../core/dom";
+// import { $ } from "../../core/dom";
 // import { ActiveRoute } from './ActiveRoute';
 
 
-export class Router {
-	constructor(selector, routes) {
-		if (!selector) throw new Error('Selector don\'t implemented in router')
+// export class Router {
+// 	constructor(selector, routes) {
+// 		if (!selector) throw new Error('Selector don\'t implemented in router')
 
-		this.$placeholder = $(selector)
-		this.routes = routes
+// 		this.$placeholder = $(selector)
+// 		this.routes = routes
 
-		this.page = null
+// 		this.page = null
 
-		this.changePageHandler = this.changePageHandler.bind(this)
+// 		this.changePageHandler = this.changePageHandler.bind(this)
 
-		this.init()
-	}
+// 		this.init()
+// 	}
 
-	init() {
-		window.addEventListener('hashchange', this.changePageHandler)
-		this.changePageHandler() // First init for check hash of page
-	}
+// 	init() {
+// 		window.addEventListener('hashchange', this.changePageHandler)
+// 		this.changePageHandler() // First init for check hash of page
+// 	}
 
-	changePageHandler() {
-		if (this.page) this.page.destroy()
-		this.$placeholder.clear()
+// 	changePageHandler() {
+// 		if (this.page) this.page.destroy()
+// 		this.$placeholder.clear()
 
-		const Page = ActiveRoute.path.includes('excel')
-			? this.routes.excel
-			: this.routes.dashboard
-		this.page = new Page(ActiveRoute.param)
+// 		const Page = ActiveRoute.path.includes('excel')
+// 			? this.routes.excel
+// 			: this.routes.dashboard
+// 		this.page = new Page(ActiveRoute.param)
 
-		this.$placeholder.append(this.page.getRoot())
-		this.page.afterRender()
-	}
+// 		this.$placeholder.append(this.page.getRoot())
+// 		this.page.afterRender()
+// 	}
 
-	destroy() {
-		window.removeEventListener('hashChange', this.changePageHandler)
-	}
-}
+// 	destroy() {
+// 		window.removeEventListener('hashChange', this.changePageHandler)
+// 	}
+// }
 
 
-export class ActiveRoute {
-	static get path() {
-		return window.location.hash.slice(1)
-	}
+// export class ActiveRoute {
+// 	static get path() {
+// 		return window.location.hash.slice(1)
+// 	}
 
-	static get param() {
-		return ActiveRoute.path.split('/')[1]
-	}
+// 	static get param() {
+// 		return ActiveRoute.path.split('/')[1]
+// 	}
 
-	static navigate(path) {
-		window.location.hash = path
-	}
-}
+// 	static navigate(path) {
+// 		window.location.hash = path
+// 	}
+// }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-
-import { Router } from './core/router/Router';
 import './scss/index.scss'
+import { $ } from "../src/core/dom";
 
-import { DashboardPage } from './pages/DashboardPage';
-import { ExcelPage } from './pages/ExcelPage';
+import { createStore } from './core/createStore';
+import { storage } from './core/utils';
+import { Excel } from './components/excel/Excel';
+import { rootReducer } from './redux/rootReducer';
+import { normalizeInitialState } from './redux/initialState';
+
+import { Header } from './components/header/Header';
+import { Table } from './components/table/Table';
 
 
-new Router('#app', {
-	dashboard: DashboardPage,
-	excel: ExcelPage
-})
+
+class Page {
+	constructor(params) {
+		this.params = params
+	}
+
+	getRoot() {
+		const params = this.params ? this.params : Date.now().toString()
+
+		const state = storage('excelT')
+		const store = createStore(rootReducer, normalizeInitialState(state))
+
+		store.subscribe(state => {
+			storage('excelT', state)
+		})
+
+		this.excel = new Excel({
+			components: [Header, Table],
+			store
+		})
+
+		return this.excel.getRoot()
+	}
+
+	afterRender() {
+		this.excel.init()
+	}
+
+	destroy() {
+		this.excel.destroy()
+	}
+}
+
+
+let page = new Page()
+$('#app').append(page.getRoot())
+page.afterRender()
 
